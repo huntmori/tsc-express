59b8c70afe54ce94c17723805fe1d369023ee289
huntmori <huntmori04x22@gmail.com>
2020-05-28 15:56:54 +0000

    fix mistyping

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,11 @@
-var express = require("express");
-var app = express();
-app.get('/', function (request, response) {
-    response.send('hello ts-node-express');
+"use strict";
+// refactoring ts
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.get('/', (request, response) => {
+    response.send('Hello world!');
 });
-app.listen(3000, function () {
-    console.log("Example app listening on port 3000!");
+app.listen(3000, () => {
+    console.log("listen 3000");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
-/*
-	var express = require("express");
-	var app = express();
-
-	app.get('/', function(request, response){
-		response.send('hello ts-node-express');
-
-	});
-
-	app.listen(3000, function(){
-		console.log("Example app listening on port 3000!");
-	});https://youtu.be/ql9-82oV2JE
-*/
 // refactoring ts
 
-import * as express from 'express'
-const app = exrpess();
+import * as express from 'express';
+const app = express();
 
 app.get('/', (request: express.Request, response : express.Response)=>{
-	res.send('Hello world!');
+	response.send('Hello world!');
 });
 
 app.listen(3000, ()=>{
